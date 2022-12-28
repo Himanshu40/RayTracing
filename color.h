ffd9947fa5035b74ebbcc8defdf452772003ae8e
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2022-12-28 12:10:21 +0530

    Add Color class alias of Vec3 class, main.cpp alias of ppm.cpp

---
+++ color.h
@@ -0,0 +1,13 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+void writeColor(std::ostream &out, Color pixelColor) {
+    out << static_cast<int>(255.99 * pixelColor.x()) << ' '
+        << static_cast<int>(255.99 * pixelColor.y()) << ' '
+        << static_cast<int>(255.99 * pixelColor.z()) << '\n';
+}
+
+#endif
