4c535b938a394a478ad8872b88e818635ab29774
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2022-12-30 10:30:09 +0530

    Added linear blend function

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
+Color rayColor(const Ray &r) {
+    Vec3 unitDirection = unitVector(r.direction());
+    auto t = 0.5 * (unitDirection.y() + 1.0);
+
+    return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+}
+
 int main() {
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspectRatio = 16.0 / 9.0;
+    const int imageWidth = 400;
+    const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+
+    // Camera
+    auto viewportHeight = 2.0;
+    auto viewportWidth = aspectRatio * viewportHeight;
+    auto focalLength = 1.0;
+
+    auto origin = Point3(0, 0, 0);
+    auto horizontal = Vec3(viewportWidth, 0, 0);
+    auto vertical = Vec3(0, viewportHeight, 0);
+    auto lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, focalLength);
 
     // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j) {
+    for (int j = imageHeight - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            Color pixelColor(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+        for (int i = 0; i < imageWidth; ++i) {
+            auto u = double(i) / (imageWidth - 1);
+            auto v = double(j) / (imageHeight - 1);
+            Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+            Color pixelColor = rayColor(r);
             writeColor(std::cout, pixelColor);
         }
     }
